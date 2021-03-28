42228851b025e9b44c5741a4ea5af399a7e2bb3b
C-D-Lewis <Bonsitm@gmail.com>
2021-03-28 16:24:44 +0100

    Pitch control

--- cpp/main.c
+++ cpp/main.c
@@ -1,15 +1,46 @@
 #include "main.h"
 
-int main() {
-  const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+const uint MOTOR_1_PIN = 2;
+const uint MOTOR_2_PIN = 3;
 
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
+/**
+ * Get frequency for a given MIDI pitch value.
+ */
+float get_pitch_freq(uint16_t pitch) {
+  if (pitch > PITCH_TABLE_SIZE) {
+    printf("Pitch not in range: %d\n", pitch);
+    return 9999999;
+  }
+
+  return pitch_table[pitch];
+};
+
+/**
+ * Tick motor 1
+ */
+void motor_1_tick(uint delay_us) {
+  gpio_put(MOTOR_1_PIN, 1);
+  sleep_us(5);
+  gpio_put(MOTOR_1_PIN, 0);
+
+  // TODO - use a timer instead for multi motors
+  sleep_us(delay_us);
+};
+
+/**
+ * Entry point.
+ */
+int main() {
+  // GPIO init
+  gpio_init(MOTOR_1_PIN);
+  gpio_init(MOTOR_2_PIN);
+  gpio_set_dir(MOTOR_1_PIN, GPIO_OUT);
+  gpio_set_dir(MOTOR_2_PIN, GPIO_OUT);
+
+  uint freq_hz = get_pitch_freq(70);
+  uint delay_us = 1000000 / freq_hz;
 
   while (true) {
-    gpio_put(LED_PIN, 1);
-    sleep_ms(250);
-    gpio_put(LED_PIN, 0);
-    sleep_ms(250);
+    motor_1_tick(delay_us);
   }
 }
--- cpp/main.h
+++ cpp/main.h
@@ -1,3 +1,4 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "pitches.h"
